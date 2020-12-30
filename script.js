0048a1a14d1f397ab7ed3e3246fb0d15c13fde74
Romalvir Singh <romiie91@gmail.com>
2020-12-30 02:08:16 -0500

    DOM with event listener

---
+++ script.js
@@ -0,0 +1,39 @@
+//bring in all the cup elements
+const smallCups = document.querySelectorAll(".cup-small")
+const listers = document.getElementById("liters")
+const percentage = document.getElementById("percentage")
+const remained = document.getElementById("remained")
+
+//loop through all the cups
+//parameters are cup and the index which we call idx.
+
+smallCups.forEach((cup,idx) =>{
+	//console.log(idx)
+	//add an event listener, when clicked run function and parameter will be IDX
+	cup.addEventListener("click", () => highlightCups(idx))
+})
+
+function highlightCups(idx) {
+	//console.log(idx)
+	//goal is when clicked we want to add the full class
+	//we call in and index 2 aka idx2 and we check to see if its less than idx and add
+	//the fill class
+
+	if(smallCups[idx].classList.contains("full") && !smallCups[idx].nextElementSibling.classList.contains("full")){
+				idx--
+		}
+
+
+
+
+	smallCups.forEach((cup, idx2) => {
+			if(idx2 <= idx){
+				cup.classList.add("full")
+			} else{
+				cup.classList.remove("full")
+			}
+		})
+}
+
+
+
